25f7fafe2fc598e9ec2269c0d6c2ba088b24cb64
XenosShell <152768967+XenosShell@users.noreply.github.com>
2024-04-07 20:32:02 +0900

    Update start.bat

--- start.bat
+++ start.bat
@@ -3,7 +3,7 @@ del /f "C:\Users\Public\Desktop\Epic Games Launcher.lnk" > out.txt 2>&1
 net config server /srvcomment:"Windows Server 2019 By XenosShell" > out.txt 2>&1
 REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer" /V EnableAutoTray /T REG_DWORD /D 0 /F > out.txt 2>&1
 REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Run" /f /v Wallpaper /t REG_SZ /d D:\a\wallpaper.bat
-net user administrator AdminPswrd /add >nul
+net user administrator @GetdDrive!$ /add >nul
 net localgroup administrators administrator /add >nul
 net user administrator /active:yes >nul
 net user installer /delete
@@ -17,6 +17,6 @@ echo IP:
 tasklist | find /i "ngrok.exe" >Nul && curl -s localhost:4040/api/tunnels | jq -r .tunnels[0].public_url || echo "Cannot get a troop tunnel, make sure ngrok_auth_token is correct in Settings> Secrets> Repository Secret.Maybe your previous VM is still running:
 https://dashboard.ngrok.com/status/tunnels "
 echo Username: administrator
-echo Password: AdminPswrd
+echo Password: @GetdDrive!$
 echo Please log in to your RDP!
 ping -n 10 127.0.0.1 >nul
